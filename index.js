811e5bcc17e9f1f62d448a68d98545cfa77eb6c2
AbdElraouf Sabri <abdelraoufsabri@gmail.com>
2022-03-31 21:26:07 +0200

    Update index.js

--- index.js
+++ index.js
@@ -28,7 +28,7 @@ const readme = `\
 </h3>
 
 <p align='center'>
-    <img src="https://readme-typing-svg.herokuapp.com?font=Syne+Mono&color=%231F1B1B&duration=4000&center=true&vCenter=true&width=530&lines=Android+Developer+%40Vodafone;Google+Certified+Associate+Android+Developer;With+experience+in+Cyber+security;Ec-council+Certified%3A+CND%2C+CEH+v10%2C+and+ECSA;And+experience+in+DevOps+and+CICD+;Studying+Jetpack+Compose+and+KMM">
+    <img src="https://readme-typing-svg.herokuapp.com?font=Syne+Mono&color=%231F1B1B&duration=4000&center=true&vCenter=true&width=530&lines=Senior+Android+Developer+%40Storm+Ideas;Google+Certified+Associate+Android+Developer;With+experience+in+Cyber+security;Ec-council+Certified%3A+CND%2C+CEH+v10%2C+and+ECSA;And+experience+in+DevOps+and+CICD+;Studying+Jetpack+Compose+and+KMM">
 </p>
 
 <!-- Social icons section -->
